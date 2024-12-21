01e3f0b0769cbe3408e126f5d43ca3a7e3a22ef2
GowthamSKdev <gowthams2647@gmail.com>
2024-12-21 20:12:07 +0530

    fix client

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-  },
+  // define: {
+  //   "process.env": process.env,
+  // },
   // server: {
   //   port: 3005,
   //   open: true,
